f68403bcf7631cd1766854f53de14a15d77591c6
shivangraikar <shivangraikar@gmail.com>
2023-05-03 02:16:26 -0400

    App.js updated

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header.js'
+import Footer from './components/Footer.js'
+import Main from './components/Main.js'
 
-function App() {
+class App extends React.Component {
+  render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          React is crazy.
-        </a>
-      </header>
-    </div>
-  );
+    <div classNAme="App">
+      <Header/>      
+
+      <Main/>
+
+      <Footer/>  
+      </div>
+    );
+  }
 }
 
 export default App;
